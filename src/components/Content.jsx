f7d66ddec084e3c1c4bb6851bd2666b89efa2079
ekavajh <javid.khasizada@ericsson.com>
2025-01-08 18:56:12 +0400

    Add base logic to update ingredients

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,9 +8,17 @@ export default function Content(props) {
   function addIngredient(e) {
     e.preventDefault();
     console.log("Added ingredient.");
+    const data = new FormData(e.currentTarget);
+    const newIngredient = data.get("ingredient");
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+
+    // NOTE: This is not interative solution. State is the way
+    // to go and will be added later.
   }
 
   const ingredients = ["Tomato", "Cheddar cheese", "Carrot"];
+
   return (
     <section className="h-[calc(100vh-100px)] flex flex-col justify-end px-24 py-8">
       <div className="flex-1 p-4 space-y-8">
@@ -22,15 +30,19 @@ export default function Content(props) {
         </ol>
       </div>
       <div className="flex mb-2 gap-2">
-        <form className="h-full flex flex-1 mb-2 gap-2">
+        <form
+          onSubmit={addIngredient}
+          className="h-full flex flex-1 mb-2 gap-2"
+        >
           <input
             type="text"
             placeholder="e.g. tomato"
             aria-label="Add ingredient"
+            name="ingredient"
             className="flex-1 px-4 text-xl rounded border border-orange-500 bg-orange-500/10"
           />
           <button
-            onClick={addIngredient}
+            type="submit"
             className="flex justify-center items-center gap-4 p-4 text-lg border-2 rounded border-orange-500 bg-orange-500/10 hover:bg-orange-400/20 font-semibold"
           >
             <CirclePlus size={24} className="text-orange-500" />
