e4939021eb61704d0a8796b710ac28afa4a0b25e
ekavajh <javid.khasizada@ericsson.com>
2025-01-08 17:37:41 +0400

    Add form tag & addtioanl attributes for accessibility

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,21 +6,24 @@ export default function Content(props) {
       <div className="flex-1">
         {/*  <!-- Empty space for ingredients and other content --> */}
       </div>
-      <div className="flex mb-2 gap-2">
-        <input
-          type="text"
-          placeholder="Add ingredient"
-          className="flex-1 h-16 px-4 text-xl rounded border border-orange-500 bg-orange-500/10"
-        />
-        <button className="flex justify-center items-center gap-4 p-4 text-lg border-2 rounded border-orange-500 bg-orange-500/10 hover:bg-orange-400/20 font-semibold">
-          <CirclePlus size={24} className="text-orange-500" />
-          Add ingredient
-        </button>
-        <button className="flex justify-center items-center gap-4 p-4 text-xl font-semibold border rounded border-orange-500 bg-orange-500 hover:bg-orange-600">
-          <CookingPot size={24} className="text-white" />
-          Cook Up!
-        </button>
-      </div>
+      <form>
+        <div className="flex mb-2 gap-2">
+          <input
+            type="text"
+            placeholder="e.g. tomato"
+            aria-label="Add ingredient"
+            className="flex-1 h-16 px-4 text-xl rounded border border-orange-500 bg-orange-500/10"
+          />
+          <button className="flex justify-center items-center gap-4 p-4 text-lg border-2 rounded border-orange-500 bg-orange-500/10 hover:bg-orange-400/20 font-semibold">
+            <CirclePlus size={24} className="text-orange-500" />
+            Add ingredient
+          </button>
+          <button className="flex justify-center items-center gap-4 p-4 text-xl font-semibold border rounded border-orange-500 bg-orange-500 hover:bg-orange-600">
+            <CookingPot size={24} className="text-white" />
+            Cook Up!
+          </button>
+        </div>
+      </form>
     </section>
   );
 }
