b78990717e721c750fa42a031ec8b310fc376bc0
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2025-01-12 13:08:55 +0400

    Add ingredients to page (#1)

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,29 +1,74 @@
+import { useState } from "react";
 import { CirclePlus, CookingPot } from "lucide-react";
+import Ingredients from "./Ingredients";
 
-export default function Content(props) {
+export default function Content() {
+  function getRecipe() {
+    console.log("Get Recipe Called!");
+  }
+
+  /*
+    This section is much more simplified in react@19.0.0
+    In new version you do not need to prevent form submission
+    manually and reset it.
+  */
+  function addIngredient(e) {
+    e.preventDefault();
+
+    const formEl = e.currentTarget;
+    const data = Object.fromEntries(new FormData(formEl));
+
+    // Validate input
+    if (data.ingredient.length === 0) {
+      alert("No ingredient provided.");
+      return;
+    }
+    if (ingredients.includes(data.ingredient)) {
+      alert("This ingredient is already added.");
+      return;
+    }
+
+    // Update ingredients
+    setIngredients((prevIngredients) => [...prevIngredients, data.ingredient]);
+
+    formEl.reset();
+  }
+
+  const [ingredients, setIngredients] = useState([]);
+
   return (
     <section className="h-[calc(100vh-100px)] flex flex-col justify-end px-24 py-8">
-      <div className="flex-1">
-        {/*  <!-- Empty space for ingredients and other content --> */}
-      </div>
-      <form>
-        <div className="flex mb-2 gap-2">
+      <Ingredients ingredients={ingredients} />
+      <div className="flex mb-2 gap-2">
+        <form
+          onSubmit={addIngredient}
+          className="h-full flex flex-1 mb-2 gap-2"
+        >
           <input
             type="text"
             placeholder="e.g. tomato"
             aria-label="Add ingredient"
-            className="flex-1 h-16 px-4 text-xl rounded border border-orange-500 bg-orange-500/10"
+            name="ingredient"
+            className="flex-1 px-4 text-xl rounded border border-orange-500 bg-orange-500/10"
           />
-          <button className="flex justify-center items-center gap-4 p-4 text-lg border-2 rounded border-orange-500 bg-orange-500/10 hover:bg-orange-400/20 font-semibold">
+          <button
+            type="submit"
+            className="flex justify-center items-center gap-4 p-4 text-lg border-2 rounded border-orange-500 bg-orange-500/10 hover:bg-orange-400/20 font-semibold"
+          >
             <CirclePlus size={24} className="text-orange-500" />
             Add ingredient
           </button>
-          <button className="flex justify-center items-center gap-4 p-4 text-xl font-semibold border rounded border-orange-500 bg-orange-500 hover:bg-orange-600">
-            <CookingPot size={24} className="text-white" />
-            Cook Up!
-          </button>
-        </div>
-      </form>
+        </form>
+        <button
+          disabled
+          type="button"
+          onClick={getRecipe}
+          className="flex justify-center items-center gap-4 p-4 text-xl font-semibold border rounded border-orange-500 bg-orange-500 hover:bg-orange-600 disabled:bg-gray-700 disabled:border-gray-300 disabled:cursor-not-allowed"
+        >
+          <CookingPot size={24} className="text-white" />
+          Cook Up!
+        </button>
+      </div>
     </section>
   );
 }
---
+++ src/components/Ingredients.jsx
@@ -0,0 +1,20 @@
+export default function Ingredients(props) {
+  let ingredientsElement;
+  if (props.ingredients.length === 0) {
+    ingredientsElement = (
+      <p className="text-gray-500">ℹ️ Added ingredients will show up here!</p>
+    );
+  } else {
+    ingredientsElement = props.ingredients.map((ingredient, index) => {
+      return <li key={index}>{ingredient}</li>;
+    });
+  }
+  return (
+    <div className="flex-1 p-4 space-y-8">
+      <h2 className="text-3xl font-semibold text-orange-500">Ingredients</h2>
+      <ol className="list-decimal list-inside text-2xl space-y-2">
+        {ingredientsElement}
+      </ol>
+    </div>
+  );
+}
